6436740b982049783667cf03af42a38cb4aeed60
Patrick McElhaney <Patrick.McElhaney@fisglobal.com>
2017-02-05 15:19:40 -0500

    wip

--- createRandomPlayer.js
+++ createRandomPlayer.js
@@ -1,13 +1,9 @@
 module.exports = function createRandomPlayer() {
-  const RandSoldiers = [];
-  let MaxSoldiers = 100;
-  for (let i = 0; i < 9; i++) {
-    const x = Math.floor(Math.random() * MaxSoldiers);
-    RandSoldiers.unshift(x);
-    MaxSoldiers -= x;
+  const RandSoldiers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < 100; i++) {
+    const x = Math.floor(Math.random() * RandSoldiers.length);
+    RandSoldiers[x]++;
   }
-
-  RandSoldiers.unshift(MaxSoldiers);
 
   return {
     name: 'random',
