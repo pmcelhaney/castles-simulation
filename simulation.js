0d16b872969e5c97dd0581d6d58b7ceb1d8003f6
Patrick McElhaney <Patrick.McElhaney@fisglobal.com>
2017-02-05 15:44:43 -0500

    wip

--- simulation.js
+++ simulation.js
@@ -28,4 +28,20 @@ function getAverages() {
 
 console.log(getAverages());
 
+GetAvegAvg();
+{
+  const averageArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const Interations = 5;
+  for (let i = 0; i < Interations + 1; i++) {
+    const NewAverage = getAverages;
+    for (let j = 0; j < 10; j++) {
+      averageArray[j] += NewAverage[j];
+    }
+  }
+  for (let i = 0; i < 10; i++) {
+    averageArray[i] /= 64;
+  }
+}
+
+
 // console.log(tournament(winners, castleGame));
