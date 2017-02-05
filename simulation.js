c43198db981b823252105929070e5a27ee56c1ea
Patrick McElhaney <Patrick.McElhaney@fisglobal.com>
2017-02-05 15:37:29 -0500

    wip

--- simulation.js
+++ simulation.js
@@ -5,23 +5,27 @@ const castleGame = require('./castleGame');
 const average = array => array.reduce((a, b) => a + b) / array.length;
 
 
-const winners = [];
+function getAverages() {
+  const winners = [];
 
-for (let j = 0; j < 64; j++) {
-  const players = [];
-  for (let i = 0; i < 64 * 64 * 16; i++) {
-    players.push(createRandomPlayer());
+  for (let j = 0; j < 64; j++) {
+    const players = [];
+    for (let i = 0; i < 64 * 64 * 16; i++) {
+      players.push(createRandomPlayer());
+    }
+    winners.push(tournament(players, castleGame));
   }
-  winners.push(tournament(players, castleGame));
+
+  const sums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let p = 0; p < 10; p++) {
+    for (let w = 0; w < winners.length; w++) {
+      sums[p] += winners[w].soldiers[p] / winners.length;
+    }
+  }
+
+  return sums;
 }
 
-const sums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-for (let p = 0; p < 10; p++) {
-  for (let w = 0; w < winners.length; w++) {
-    sums[p] += winners[w].soldiers[p] / winners.length;
-  }
-}
-
-console.log(sums);
+console.log(getAverages());
 
 // console.log(tournament(winners, castleGame));
